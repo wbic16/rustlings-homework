9ffd3e302b04a160cdb14b747f0aed02d7518fb2
Will Bickford <wbic16@gmail.com>
2024-09-22 17:12:50 -0500

    Solved 26/94

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -1,8 +1,6 @@
 fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
-    let mut vec = vec;
-
+    let mut vec: Vec<i32> = vec;
     vec.push(88);
-
     vec
 }
 
@@ -14,13 +12,11 @@ fn main() {
 mod tests {
     use super::*;
 
-    // TODO: Make both vectors `vec0` and `vec1` accessible at the same time to
-    // fix the compiler error in the test.
     #[test]
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let vec1 = fill_vec(vec0.clone());
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
