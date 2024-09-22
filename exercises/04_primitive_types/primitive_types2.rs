2bed87dee13c797648dffb8d758d05183bf328f0
Will Bickford <wbic16@gmail.com>
2024-09-22 16:58:08 -0500

    Solved 18/94 and Snuck a Wild Phext Delimiter In

--- exercises/04_primitive_types/primitive_types2.rs
+++ exercises/04_primitive_types/primitive_types2.rs
@@ -1,5 +1,12 @@
 // Characters (`char`)
 
+fn is_phext_delimiter(letter_in: char) -> bool {
+    let letter:u8 = letter_in as u8;
+    
+    letter == 0x17 || letter == 0x18 || letter == 0x19 ||
+    letter == 0x1A || letter == 0x1C || letter == 0x1D ||
+    letter == 0x1E || letter == 0x1F || letter == 0x01
+}
 fn main() {
     // Note the _single_ quotes, these are different from the double quotes
     // you've been seeing around.
@@ -12,16 +19,14 @@ fn main() {
         println!("Neither alphabetic nor numeric!");
     }
 
-    // TODO: Analogous to the example before, declare a variable called `your_character`
-    // below with your favorite character.
-    // Try a letter, try a digit (in single quotes), try a special character, try a character
-    // from a different language than your own, try an emoji 😉
-    // let your_character = '';
+    let your_character = '\x17';
 
     if your_character.is_alphabetic() {
         println!("Alphabetical!");
     } else if your_character.is_numeric() {
         println!("Numerical!");
+    } else if is_phext_delimiter(your_character) {
+        println!("A Wild Phext Delimiter!");
     } else {
         println!("Neither alphabetic nor numeric!");
     }
