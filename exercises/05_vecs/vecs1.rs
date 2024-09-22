37c1fddc583560d1190962ba651e5c3399309414
Will Bickford <wbic16@gmail.com>
2024-09-22 17:06:56 -0500

    Solved 23/94

--- exercises/05_vecs/vecs1.rs
+++ exercises/05_vecs/vecs1.rs
@@ -1,9 +1,7 @@
 fn array_and_vec() -> ([i32; 4], Vec<i32>) {
     let a = [10, 20, 30, 40]; // Array
 
-    // TODO: Create a vector called `v` which contains the exact same elements as in the array `a`.
-    // Use the vector macro.
-    // let v = ???;
+    let v = vec!(a[0], a[1], a[2], a[3]);
 
     (a, v)
 }
